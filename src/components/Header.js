f84ee11ac1bd664975277974061ec76369c99ef1
Justin <justin_jang@brown.edu>
2024-06-25 23:45:18 -0700

    additional

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,13 +5,10 @@ const Header = ({toggleModal, numberOfAthletes}) => {
   return (
     <header className='header'>
         <div className='container'>
-            <h3>Athlete List ({numberOfAthletes})</h3>
+            <button className = 'btn'>Athlete List ({numberOfAthletes})</button>
             <button onClick={() => toggleModal(true)} className='btn'>
                 <i class='bi-plus-square'></i>
-                {/* <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square-fill" viewBox="0 0 16 16">
-  <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0"/>
-</svg> */}
-                Add New Rookie
+                Add New Player
             </button>
         </div>
     </header>
