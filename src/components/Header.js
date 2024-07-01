7d6559a0f6c480505cea351906f95328452437a5
Justin <justin_jang@brown.edu>
2024-06-30 22:43:31 -0700

    animations

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import '../index.css';
+import 'animate.css'
 
 const Header = ({toggleModal, numberOfAthletes}) => {
   return (
     <header className='header'>
         <div className='container'>
             <button className = 'btn'>Athlete List ({numberOfAthletes})</button>
-            <p className='bruh'>hi</p>
+            <p className='bruh'>Welcome to the NBA Learner's Guide!</p>
             <button onClick={() => toggleModal(true)} className='btn'>
                 <i class='bi-plus-square'></i>
                 Add New Player
